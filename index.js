94b8d7a48aedcf55799063fd72c787f8bf906dfe
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-13 22:39:51 +0530

    commit

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ const colors = require("colors");
 const ejsMate = require("ejs-mate")
 const methodOverride = require("method-override")
 const port = 3000;
+const session= require('express-session');
+const flash=require('connect-flash');
+
 
 mongoose.connect("mongodb://127.0.0.1:27017/ecomm-careerbootcamp")
 .then(()=> console.log("db connected sucessfully".blue))
 .catch((err)=> console.log(err));
 
+const sessionConfig={
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie:{}
+}
 //Routes
   const productRoutes = require("./routes/productRoutes");
   const reviewRoutes = require("./routes/reviewRoutes")
@@ -21,6 +30,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
+app.use(express.session(sessionConfig))
+app.use(flash());
 
 
 // routes middleware
