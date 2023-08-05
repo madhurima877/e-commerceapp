a3a92a9486dec20f62395002fd3685abde1868b5
madhurima999 <gorainmadhurima999@gmail.com>
2023-08-06 01:48:33 +0530

    h

--- models/User.js
+++ models/User.js
@@ -1,4 +1,3 @@
-
 const mongoose = require("mongoose");
 const passportLocalMongoose = require('passport-local-mongoose');
 
@@ -8,7 +7,25 @@ const userSchema = new mongoose.Schema({
     // username : String, ---> This is automatically added by passport-local-mongoose, no need to add ourselves
     // password : String,  ---> This is automatically added by passport-local-mongoose
 
-    email : String
+    email : String,
+
+    cart: [
+        {
+            name: String,
+            price: Number,
+            img: String,
+            id: mongoose.Schema.Types.ObjectId,
+            count : {
+                type:Number,
+                default: 1,
+                min: [0, "Quantity cannot ne less than 1"]
+            }
+
+        }
+
+
+
+    ]
 })
 
 
